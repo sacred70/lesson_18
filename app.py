1e6c342df81889dccda676b1bb44340dc3feb869
sacred70 <sacred70@mail.ru>
2023-02-10 18:23:14 +0700

    1

--- app.py
+++ app.py
@@ -1,46 +1,32 @@
-# основной файл приложения. здесь конфигурируется фласк, сервисы, SQLAlchemy и все остальное что требуется для приложения.
-# этот файл часто является точкой входа в приложение
-
-# Пример
-
 from flask import Flask
 from flask_restx import Api
-
 from config import Config
-from models import Review, Book
 from setup_db import db
-from views.books import book_ns
-from views.reviews import review_ns
+from views.movies import movies_ns
+from views.directors import director_ns
+from views.genres import genre_ns
 
-# функция создания основного объекта app
+
 def create_app(config_object):
+    """Принимает объект класса Config, возвращает приложение"""
     app = Flask(__name__)
     app.config.from_object(config_object)
-    register_extensions(app)
+    register_extension(app)
     return app
 
 
-# функция подключения расширений (Flask-SQLAlchemy, Flask-RESTx, ...)
-def register_extensions(app):
+def register_extension(app):
+    """Принимает приложение, инициализирует базу данных и регистрирует неймспейсы"""
     db.init_app(app)
     api = Api(app)
-    api.add_namespace(...)
-    create_data(app, db)
-
-
-# функция создать несколько сущностей, чтобы добавить их в БД
-def create_data(app, db):
+    api.add_namespace(movies_ns)
+    api.add_namespace(director_ns)
+    api.add_namespace(genre_ns)
     with app.app_context():
         db.create_all()
 
 
-
-        with db.session.begin():
-            db.session.add_all("""здесь список созданных объектов""")
-
-
 app = create_app(Config())
-app.debug = True
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=10001, debug=True)
+    app.run()
--- implemented.py
+++ implemented.py
@@ -1,7 +1,18 @@
 # файл для создания DAO и сервисов чтобы импортировать их везде
 
-# book_dao = BookDAO(db.session)
-# book_service = BookService(dao=book_dao)
-#
-# review_dao = ReviewDAO(db.session)
-# review_service = ReviewService(dao=review_dao)
+from dao.director import DirectorDAO
+from dao.genre import GenreDAO
+from dao.movie import MovieDAO
+from service.directors import DirectorService
+from service.genres import GenreService
+from service.movies import MovieService
+from setup_db import db
+
+movie_dao = MovieDAO(db.session)
+movie_service = MovieService(movie_dao)
+
+director_dao = DirectorDAO(db.session)
+director_service = DirectorService(director_dao)
+
+genre_dao = GenreDAO(db.session)
+genre_service = GenreService(genre_dao)
