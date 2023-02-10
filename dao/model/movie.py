1b1ed5e5b50678cfeece9c66e096dbad59b13e5e
sacred70 <sacred70@mail.ru>
2023-02-10 14:18:56 +0700

    1

---
+++ dao/model/movie.py
@@ -0,0 +1,18 @@
+from setup_db import db
+from marshmallow import Schema, fields
+
+
+# Модель фильма и схема сериализации
+class Movie(db.Model):
+    __tablename__ = 'movie'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    descriptions = db.Column(db.String)
+    trailer = db.Column(db.String)
+    year = db.Column(db.Integer)
+    rating = db.Column(db.Float)
+    genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
+    genre = db.relationship("Genre")  #??????????
+    director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
+    director = db.relationship("Director")  #??????????
+
--- setup_db.py
+++ setup_db.py
@@ -1,5 +1,3 @@
-
-
-# from flask_sqlalchemy import SQLAlchemy
-#
-# db = SQLAlchemy()
+from flask_sqlalchemy import SQLAlchemy
+#  инициализируем БД
+db = SQLAlchemy()
